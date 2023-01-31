6b316cdb4ac82721dd8721144e484a3e880ed5c4
Marc Newman <marc.anthony.newman@gmail.com>
2023-01-31 10:34:11 -0500

    iterating_Arrays.js

---
+++ JS/iterating.js
@@ -0,0 +1,105 @@
+(function(){
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+
+var names = ['zebra', 'lion', 'dog', 'cat'];
+    console.log('There are ' + names.length + ' names in the array');
+
+    console.log('The first name is: ' + names[0]);
+    console.log('The second name is: ' + names[1]);
+    console.log('The third name is: ' + names[2]);
+    console.log('The fourth name is: ' + names[3]);
+
+    for (var i = 0; i < names.length; i++){
+        console.log('The name at index ' + i + ' is: ' + names[i]);
+    }
+
+    names.forEach(function(names){
+        console.log('Here is a lovely pet: ' + names + ' ');
+    })
+
+    var names(['zebra', 'lion', 'dog', 'cat'])
+    if
+    //
+    // > first([1, 2, 3, 4, 5]) // returns 1
+    // > second([1, 2, 3, 4, 5]) // returns 2
+    // > last([1, 2, 3, 4, 5]) // return 5
+//     var shapes = ['square', 'rectangle', 'circle', 'triangle'];
+//
+// // loop through the array and log the values
+//     shapes.forEach(function(shape) {
+//         console.log('Here is a lovely shape: ' + shape + '.');
+//     });
+
+
+//     var shapes = ['square', 'rectangle', 'circle', 'triangle'];
+//
+// // loop through the array and log the values
+//     for (var i = 0; i < shapes.length; i++) {
+//         console.log('The shape at index ' + i + ' is: ' + shapes[i]);
+//     }
+
+
+
+//     var shapes = ['square', 'rectangle', 'circle', 'triangle'];
+//
+//     console.log('There are ' + shapes.length + ' shapes in the array');
+//
+//     console.log('The first shape is: ' + shapes[0]);
+// // The first shape is: square
+//
+//     console.log('The second shape is: ' + shapes[1]);
+// // The second shape is: rectangle
+//
+//     console.log('The third shape is: ' + shapes[2]);
+// // The third shape is: circle
+//
+//     console.log('The fourth shape is: ' + shapes[3]);
+// // The fourth shape is: triangle
+//
+//     console.log('The fifth shape is: ' + shapes[4]);
+// // The fifth shape is: undefined
+
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+
+})();
