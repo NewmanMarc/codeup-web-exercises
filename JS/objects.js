704c5750fb29762955bdcbf1df61a3e9ce7fa43f
Marc Newman <marc.anthony.newman@gmail.com>
2023-02-02 16:32:43 -0500

    updated objects exercise

---
+++ JS/objects.js
@@ -0,0 +1,19 @@
+var myTruckObject = {};
+
+console.log(typeof myTruckObject);
+console.log(myTruckObject);
+
+myTruckObject.make = "Ford";
+
+console.log("How does my object look now?");
+console.log(myTruckObject);
+
+myTruckObject["model"] = "Silverado";
+
+console.log("How does my object look now? 2nd time");
+
+myTruckObject.year = [2015];
+
+myTruckObject.Owner = {firstName:"Marc" , lastName: "Newman"};
+
+
